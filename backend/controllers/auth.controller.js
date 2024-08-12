d2a5b1c5b4eae376f497177fc3b14a54f0207e1e
yusufinann <yusuf.inan2@ogr.sakarya.edu.tr>
2024-08-12 20:05:50 +0300

    connected database,completed signup endpoint with validation and user creation

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,14 +1,57 @@
-export const signup = (req, res) => {
-    console.log("signupUser");
-    res.send("Signup successful");
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Password dont match" });
+    }
+
+    const user = await User.findOne({ username });
+    if (user) {
+      return res.status(400).json({ error: "Username already exist" });
+    }
+
+    //HASH Password here
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //https://avatar-placeholder.iran.liara.run/
+    const boyProfilePic = `https://avatar-placeholder.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar-placeholder.iran.liara.run/public/girl?username=${username}`;
+
+    //Create an user object
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+    if (newUser) {
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "internal server error" });
+  }
 };
 
 export const login = (req, res) => {
-    console.log("loginUser");
-    res.send("Login successful");
+  console.log("loginUser");
+  res.send("Login successful");
 };
 
 export const logout = (req, res) => {
-    console.log("logoutUser");
-    res.send("Logout successful");
+  console.log("logoutUser");
+  res.send("Logout successful");
 };
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js'; // Yolu doğru belirttiğinizden emin olun
+import connectToMongoDB from './db/connectToMongoDB.js';
 
 dotenv.config(); // Load environment variables from .env file
 
-const app = express();
+const app = express(); // `app` nesnesi burada tanımlandı
+
+app.use(express.json()); // `app` tanımlandıktan sonra kullanılabilir
+
+// Use the `authRoutes` under `/api/auth` prefix
+app.use("/api/auth", authRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-// Use the `authRoutes` under `/api/auth` prefix
-app.use("/api/auth", authRoutes);
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`);
+});
