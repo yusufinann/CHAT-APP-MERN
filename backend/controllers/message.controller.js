d6908496c4d1947297bea733d1fc143b49a8835f
yusufinann <yusuf.inan2@ogr.sakarya.edu.tr>
2024-08-14 15:38:15 +0300

    completed getMessages route and method and tested

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,7 +1,7 @@
 import Conversation from "../models/conversation.model.js"; // Import the Conversation model
 import Message from "../models/message.model.js"; // Import the Message model
 
-export const sendMessage = async (req, res) => {
+export const sendMessages = async (req, res) => {
   try {
     const { message } = req.body;
     const { id: receiverId } = req.params;
@@ -40,4 +40,24 @@ export const sendMessage = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
   // console.log("Message sent",req.params.id)
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages");
+    
+    if(!conversation){
+      return res.status(200).json([]);
+    }
+    const messages=conversation.messages
+    res.status(200).json(messages);
+    // console.log("Message got : ",req.params.id)
+  } catch (error) {
+    console.log("Error in getMessage controller:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,7 +1,8 @@
 import express from "express"
-import {sendMessage} from   "../controllers/message.controller.js";
+import {getMessages, sendMessages} from   "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 const router=express.Router();
 
-router.post("/send/:id",protectRoute,sendMessage) //protectRoute controls whether user login before send message
+router.get("/:id",protectRoute,getMessages)
+router.post("/send/:id",protectRoute,sendMessages) //protectRoute controls whether user login before send message
 export default router
