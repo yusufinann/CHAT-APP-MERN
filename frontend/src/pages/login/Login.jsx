40f819fd51b301223bb1dcaab59017f338473d90
yusufinann <yusuf.inan2@ogr.sakarya.edu.tr>
2024-08-16 12:12:15 +0300

    completed login operation and login validations.

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,8 +1,17 @@
 //import React from 'react'
 
+import { useState } from "react"
 import { Link } from "react-router-dom"
-
+import useLogin from "../../hooks/useLogin";
 const Login = () => {
+	const [username,setUsername]=useState("")
+	const [password,setPassord]=useState("");
+
+	const{loading,login}= useLogin();
+	const handleSubmit=async (e)=>{
+		e.preventDefault();
+		await login(username,password)
+	}
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
@@ -11,7 +20,7 @@ const Login = () => {
 					<span className='text-blue-500'> ChatApp</span>
 				</h1>
 
-				<form>
+				<form onSubmit={handleSubmit}>
 					<div>
 						<label className='label p-2'>
 							<span className='text-base label-text'>Username</span>
@@ -20,7 +29,8 @@ const Login = () => {
 							type='text'
 							placeholder='Enter username'
 							className='w-full input input-bordered h-10'
-							
+							value={username}
+							onChange={(e)=>setUsername(e.target.value)}
 						/>
 					</div>
 
@@ -32,7 +42,8 @@ const Login = () => {
 							type='password'
 							placeholder='Enter Password'
 							className='w-full input input-bordered h-10'
-							
+							value={password}
+							onChange={(e)=>setPassord(e.target.value)}
 						/>
 					</div>
 					 <Link to='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
@@ -41,7 +52,11 @@ const Login = () => {
 
 					<div>
 						<button className='btn btn-block btn-sm mt-2' >
-						Login
+						{loading ? (
+                    <span className="loading loading-spinner"></span>
+            ) : (
+                "Login"
+            )}
 						</button>
 					</div>
 				</form>
