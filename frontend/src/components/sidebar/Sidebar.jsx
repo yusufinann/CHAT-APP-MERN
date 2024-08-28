93ed81da5585f02c655e7a9e81698f64cfff7432
yusufinann <yusuf.inan2@ogr.sakarya.edu.tr>
2024-08-28 17:12:22 +0300

    edited  path

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import Conversations from "./Conversations"
-import LogoutButton from "./LogoutButton.JSX"
+import LogoutButton from "./LogoutButton"
 import SearchInput from "./SearchInput"
 
 const Sidebar = () => {
