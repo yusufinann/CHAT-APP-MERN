72011c8b9d303ec85e6f30be55e2e95060971fed
yusufinann <yusuf.inan2@ogr.sakarya.edu.tr>
2024-08-28 17:05:02 +0300

    edited sidebar name

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 
 import MessageContainer from '../../components/messages/MessageContainer'
-import Sidebar from '../../components/sidebar/SideBar';
+import Sidebar from '../../components/sidebar/Sidebar';
 const Home = () => {
 	return (
 		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
